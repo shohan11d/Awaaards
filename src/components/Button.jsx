0b1025d8ba6157bf801c46f4617e69de06876516
shohan11d <shohan11d@gmail.com>
2025-12-31 08:17:58 +0600

    init again

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
-function Button({title, id, rightIcon, leftIcon, containerClass}) {
+function Button({ title, id, rightIcon, leftIcon, containerClass }) {
   return (
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
-    {leftIcon}
-    <span className="relative infine-flex overflow-hidden font-general text-xs  uppercase">
-      <div>
-        {title}
-      </div>
-    </span>
-    {rightIcon}
+      {leftIcon}
+      <span className="relative infine-flex overflow-hidden font-general text-xs  uppercase">
+        <div>
+          {title}
+        </div>
+      </span>
+      {rightIcon}
     </button>
   )
 }
