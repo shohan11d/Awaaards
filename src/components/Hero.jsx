75b18717e3bda1d984cec7990c21758376035b2e
shohan11d <shohan11d@gmail.com>
2026-01-05 15:15:38 +0600

    hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,74 @@
 import { TiLocationArrow } from "react-icons/ti";
 import Button from "./Button";
+import { useState } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Hero() {
-  return (
-    <div className="relative px-4 h-dvh overflow-hidden">
-      <div className=" h-dvh w-full flex flex-col justify-between container mx-auto size-full relative z-10">
-        <div className="py-8 mt-16 md:mt-24">
-          <img
-            src="./img/rivals-logo.png"
-            className="w-40 md:w-100 mb-4"
-            alt=""
-          />
-          <p className="text-white text-xs md:text-2xl  uppercase text-blue-50">
-            The Super hero team-based pvp shooter
-          </p>
-          <p className="text-[#FBDB2B] text-xs md:text-2xl font-bold uppercase mb-4">
-            All heroes are free to play!
-          </p>
-          <Button
-            id="watch-trailer"
-            title="Watch Trailer"
-            link="https://www.youtube.com/watch?v=-b0veB7q9P4"
-            leftIcon={<TiLocationArrow />}
-            containerClass="bg-[#FBDB2B] text-black flex-center gap-1 self-start"
-          />
-        </div>
+  const [loading, setLoading] = useState(false);
 
-        <div className="flex items-center justify-end">
-          <h1 className="hero-heading text-white mb-8">Play Now</h1>
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+  return (
+    <div className="relative">
+      <div
+        id="video-frame"
+        className="relative px-4 h-dvh overflow-hidden z-20"
+      >
+        <div className=" h-dvh w-full flex flex-col justify-between container mx-auto size-full relative z-10">
+          <div className="py-8 mt-16 md:mt-24">
+            <img
+              src="./img/rivals-logo.png"
+              className="w-40 md:w-100 mb-4"
+              alt=""
+            />
+            <p className=" text-xs md:text-2xl  uppercase text-blue-50">
+              The Super hero team-based pvp shooter
+            </p>
+            <p className="text-[#FBDB2B] text-xs md:text-2xl font-bold uppercase mb-4">
+              All heroes are free to play!
+            </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              link="https://www.youtube.com/watch?v=-b0veB7q9P4"
+              leftIcon={<TiLocationArrow />}
+              containerClass="bg-[#FBDB2B] text-black flex-center gap-1 self-start"
+            />
+          </div>
+          <h1 className="absolute right-4 bottom-0 md:right-30 lg:right-44  z-40 text-white hero-heading  mb-8">
+            Play Now
+          </h1>
         </div>
+        <video
+          src="./videos/marvel-hero1.mp4"
+          autoPlay
+          muted
+          loop
+          className="w-full inset-0  size-full absolute top-0 left-0 object-cover"
+        />
       </div>
-      <video
-        src="./videos/marvel-hero1.mp4"
-        autoPlay
-        muted
-        loop
-        className="w-full inset-0  size-full absolute top-0 left-0 object-cover"
-      />
+      <h1 className="absolute right-6 bottom-0 md:right-28 lg:right-87  z-10 text-black hero-heading  mb-8">
+        Play Now
+      </h1>
     </div>
   );
 }
